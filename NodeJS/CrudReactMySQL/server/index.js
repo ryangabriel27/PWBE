c0e0a30a370c56c5b3403698f6afbf0602c96246
ryangabriel27 <ryanjulio56@gmail.com>
2023-10-27 16:52:41 -0300

    27.10 client vazio

--- NodeJS/CrudReactMySQL/server/index.js
+++ NodeJS/CrudReactMySQL/server/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
-const dataBase = require("mysql");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"produtoscrud"
+});
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 app.listen(3001, ()=>{
     console.log("Rodando o servidor");
 });
 
-app.get('/', (req,res)=>{
-    res.send("Hello World!")
-});
+// app.get('/', (req,res)=>{
+    
+//     let sql = "INSERT INTO produtos( id,name,cost,category ) VALUES ( 1,'Racao Cachorro 10KG','80','Cachorro' )";
+
+//     db.query(sql,(err,result)=>{
+//         console.log(err);
+//     });
+// });
