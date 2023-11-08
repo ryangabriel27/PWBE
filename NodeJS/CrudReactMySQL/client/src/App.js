e259117e3af3544f9516f8d685651e5d5695f577
ryangabriel27 <ryanjulio56@gmail.com>
2023-11-08 14:09:20 -0300

    08.11

--- NodeJS/CrudReactMySQL/client/src/App.js
+++ NodeJS/CrudReactMySQL/client/src/App.js
@@ -4,17 +4,22 @@ import Axios from "axios";
 import Card from './components/cards/card.js';
 
 function App() {
+  // Estado para armazenar os valores inseridos pelo usuário
   const [values, setValues] = useState();
+
+  // Estado para armazenar a lista de produtos recuperada do servidor
   const [listProducts, setListProducts] = useState([]);
 
   console.log(listProducts);
 
+  // Função para atualizar o estado 'values' conforme o usuário digita nos campos
   const handleAddValues = (value) => {
     setValues((prevValue) => ({
       ...prevValue, [value.target.name]: value.target.value,
     }));
   }
 
+  // Função para enviar os dados inseridos pelo usuário para o servidor
   const handleRegisterButton = () => {
     Axios.post("http://localhost:3001/cadastro", {
       name: values.name,
@@ -23,12 +28,14 @@ function App() {
     }).then((response) => {
       console.log(response);
     });
+    // Recarrega a página após o cadastro
     document.location.reload();
   }
 
+  // Efeito de carregamento inicial para buscar a lista de produtos do servidor
   useEffect(() => {
     Axios.get("http://localhost:3001/getCards").then((response) => {
-      setListProducts(response.data)
+      setListProducts(response.data);
     })
   }, []);
 
@@ -37,17 +44,18 @@ function App() {
       <div className='register-container'>
         <h1 className='register-title'>Pet&Repet</h1>
 
+        {/* Inputs para o usuário inserir os dados do produto */}
         <input className='register-input' type='text' name='name' placeholder='Nome do produto' onChange={handleAddValues}></input>
-
         <input className='register-input' type='text' name='cost' placeholder='Preço do produto' onChange={handleAddValues}></input>
-
         <input className='register-input' type='text' name='category' placeholder='Categoria' onChange={handleAddValues}></input>
 
+        {/* Botão para registrar o produto no banco de dados */}
         <button onClick={handleRegisterButton} className="register-button">
           Cadastrar
         </button>
       </div>
 
+      {/* Mapeia a lista de produtos e exibe cada um como um componente 'Card' */}
       {listProducts.map((value) => {
         return <Card
           key={value.id}
--- NodeJS/CrudReactMySQL/server/index.js
+++ NodeJS/CrudReactMySQL/server/index.js
@@ -1,55 +1,78 @@
+// Importação de bibliotecas necessárias
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 
+// Criação de uma conexão com o banco de dados MySQL
 const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"produtoscrud"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "produtoscrud"
 });
 
+// Inicialização de um servidor Express
 const app = express();
 
+// Habilita o uso do middleware CORS para lidar com solicitações de diferentes origens
 app.use(cors());
+
+// Habilita o middleware para análise de JSON no corpo das solicitações
 app.use(express.json());
 
-app.post("/cadastro", (req,res)=>{
+// Endpoint para lidar com solicitações POST de cadastro de produtos
+app.post("/cadastro", (req, res) => {
     const { name } = req.body;
     const { cost } = req.body;
     const { category } = req.body;
 
-    let sql = "INSERT INTO produtos( name,cost,category ) VALUES (?,?,?)"
+    // Cria uma consulta SQL de inserção
+    let sql = "INSERT INTO produtos(name, cost, category) VALUES (?,?,?)";
 
-    db.query(sql,[name, cost, category],(err,result) => {
-        console.log(err);
+    // Executa a consulta SQL com os valores fornecidos no corpo da solicitação
+    db.query(sql, [name, cost, category], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
     });
-})
+});
 
-app.get("/getCards", (req,res)=>{
-    
+// Endpoint para lidar com solicitações GET para buscar todos os produtos
+app.get("/getCards", (req, res) => {
+    // Cria uma consulta SQL para selecionar todos os registros da tabela 'produtos'
     let sql = "SELECT * FROM produtos";
 
-    db.query(sql, (err,result)=>{
-        if (err) console.log(err);
-        else res.send(result);
-    })
-})
+    // Executa a consulta SQL e envia os resultados como resposta
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
 
-app.listen(3001, ()=>{
+// Inicia o servidor na porta 3001
+app.listen(3001, () => {
     console.log("Rodando o servidor");
 });
 
-app.put("/edit", (req,res) => {
+// Endpoint para lidar com solicitações PUT de edição de produtos
+app.put("/edit", (req, res) => {
     const { id } = req.body;
     const { name } = req.body;
     const { cost } = req.body;
     const { category } = req.body;
 
-    let sql = "UPDATE produtos name = ?, cost = ?, category = ?, WHERE id = ?"
+    // Cria uma consulta SQL de atualização com base no ID fornecido
+    let sql = "UPDATE produtos SET name = ?, cost = ?, category = ? WHERE id = ?";
 
-    db.query(sql, (err,result)=>{
-        if (err) console.log(err);
-        else res.send(result);
-    })
-})
+    // Executa a consulta SQL com os valores fornecidos no corpo da solicitação
+    db.query(sql, [name, cost, category, id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
