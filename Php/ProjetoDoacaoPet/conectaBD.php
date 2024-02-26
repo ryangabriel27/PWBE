959f4f078000fa779c62f2aacfa966551b74a821
ryangabriel27 <ryanjulio56@gmail.com>
2024-02-26 17:20:42 -0300

    ai

---
+++ Php/ProjetoDoacaoPet/conectaBD.php
@@ -0,0 +1,34 @@
+<?php
+// endereco
+// nome do BD
+// usuario
+// senha
+$endereco = 'localhost';
+$banco = 'postgres';
+$admin = 'postgres';
+$senha = 'postgres';
+try {
+    // sgbd:host;port;dbname
+    // admin
+    // senha
+    // errmode
+    $pdo = new PDO(
+        "pgsql:host=$endereco;port=5432;dbname=$banco",
+        $admin,
+        $senha,
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+    echo "Conectado no banco de dados!!!";
+    $sql = "CREATE TABLE IF NOT EXISTS 
+    usuarios (ID SERIAL, 
+    NOME VARCHAR(255), 
+    DATA_NASCIMENTO VARCHAR(255), 
+    TELEFONE VARCHAR(255), 
+    EMAIL VARCHAR(255) PRIMARY KEY, 
+    SENHA VARCHAR(255))";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+} catch (PDOException $e) {
+    echo "Falha ao conectar ao banco de dados. <br/>";
+    die($e->getMessage());
+}
